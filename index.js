2e2cb7c85cf48743e6f075784e0d62cf1d1d264c
+ <erol.hendrickson@gmail.com>
2022-06-13 06:49:47 -0500

    commit tree 0844f92ab9add55931c7e72b61e6f67a7d449431

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+const userData = { name: "Steve",
+ email: "steve@steve.com",
+}
+
+
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData)})
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (object) {
+        document.body.append(object.id);
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access")
+        document.body.append(error.message);
+    })
+};
+
+
+submitData()
+
+
+
